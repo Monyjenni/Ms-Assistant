62bb2f66e0689d81ba600fe7280893b9366a07f5
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-07-02 23:08:36 +0700

    feat: Student Migration

--- ms-assistant-backend/database/migrations/2024_07_01_040517_create_students_table.php
+++ ms-assistant-backend/database/migrations/2024_07_01_040517_create_students_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
-            $table->string('last_name')->nullable();
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('phone_number');
             $table->string('password');
@@ -24,7 +24,6 @@ return new class extends Migration
             $table->string('faculty');
             $table->timestamps();
         });
-
     }
 
     /**
